30340c8297f345319ce3cbdadb5b0d2b3f777126
sayaligurav7789 <sayligurab7789@gmail.com>
2026-04-15 19:31:30 +0530

    Added a new route: GET /api/users/:clerkId/streak pointing to that new function

--- Server/src/routes/user.routes.js
+++ Server/src/routes/user.routes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getUserSessions } from '../controllers/user.controller.js';
+import { getUserSessions, getUserStreak } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-// Get all sessions for a specific user (by Clerk ID)
 router.get('/:clerkId/sessions', getUserSessions);
+router.get('/:clerkId/streak', getUserStreak);
 
 export default router;
